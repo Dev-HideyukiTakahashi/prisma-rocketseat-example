e586ee103eafbab0a7557245de8722fc31719404
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-06-27 19:07:52 -0300

    api base

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { questionsRoutes } from './questions-routes';
+import { usersRoutes } from './users-routes';
+
+const routes = Router();
+routes.use('/questions', questionsRoutes);
+routes.use('/users', usersRoutes);
+
+export { routes };
---
+++ src/routes/questions-routes.ts
@@ -0,0 +1,12 @@
+import { QuestionsController } from '@/controllers/questions-controller';
+import { Router } from 'express';
+
+const questionsRoutes = Router();
+const questionsController = new QuestionsController();
+
+questionsRoutes.get('/', questionsController.index);
+questionsRoutes.post('/', questionsController.create);
+questionsRoutes.put('/:id', questionsController.update);
+questionsRoutes.delete('/:id', questionsController.remove);
+
+export { questionsRoutes };
